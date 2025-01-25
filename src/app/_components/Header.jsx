bc1138b48a09c26fd556a4f65dceafe8e4cabfc8
longman14 <hassandamilola580@gmail.com>
2025-01-25 22:01:46 +0000

    bug fix

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -6,7 +6,12 @@ function Header() {
   return (
     <div className="flex justify-between items-center px-5 py-1 shadow-md">
       <Image src={'/logo.svg'} width={150} height={50} alt="Logo" />
-      <Button>Get Started</Button>
+      <a
+          className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-secondary focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+          href="/dashboard"
+        >
+          Get Started
+        </a>
     </div>
   );
 }
