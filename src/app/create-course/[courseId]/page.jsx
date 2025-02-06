52333a2dc53812864605d8dc992c976254a246f4
longman14 <hassandamilola580@gmail.com>
2025-02-06 22:14:04 +0000

    worked on generate chapter content

--- src/app/create-course/[courseId]/page.jsx
+++ src/app/create-course/[courseId]/page.jsx
@@ -8,10 +8,13 @@ import CourseBasicInfo from "./_components/CourseBasicInfo";
 import CourseDetail from "./_components/CourseDetail";
 import ChapterList from "./_components/ChapterList";
 import { Button } from "@/components/ui/button";
+import { GenerateCourseContent_AI } from "../../../../configs/AiModels";
+import LoadingDialog from "../_component/LoadingDialog";
 
 function CoursePage({ params }) {
   const { user } = useUser();
   const [course, setCourse] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     params && GetCourse();
@@ -34,18 +37,39 @@ function CoursePage({ params }) {
       console.error("Error fetching course:", error);
     }
   };
-// Generating chapter content
-const GenerateChapterContent =()=>{
-  const chapters=course?.courseOutput?.chapters;
-  chapters.forEach((chapter, index)=>{
-    const PROMPT = 'Explain the concept in detail on Topic: '+course?.courseOutput?.courseName+', Chapter: '+chapter?.chapterName+' in JSON format with list of array with field as title, explanation on given chapter in detail, Code example (code field <precode> format) if applicable';
-    console.log(PROMPT); 
-  })
-}
+  // Generating chapter content
+  const GenerateChapterContent = () => {
+    setLoading(true);
+    const chapters = course?.courseOutput?.chapters;
+    chapters.forEach(async (chapter, index) => {
+      const PROMPT =
+        "Explain the concept in detail on Topic: " +
+        course?.courseOutput?.courseName +
+        ", Chapter: " +
+        chapter?.chapterName +
+        " in JSON format with list of array with field as title, explanation on given chapter in detail, Code example (code field <precode> format) if applicable";
+      console.log(PROMPT);
+      if (index === 0) {
+        try {
+          const result = await GenerateCourseContent_AI.sendMessage(PROMPT);
+          console.log(result.response?.text());
+
+          //Generate Video Url
+
+          //Save Chapter Content & Video Url
+          setLoading(false);
+        } catch (e) {
+          setLoading(false);
+
+          console.error("Error generating chapter content:", e);
+        }
+      }
+    });
+  };
   return (
     <div className="mt-10 px-7 md:px-20 lg:px:44">
       <h2 className="text-center font-bold text-2xl">Course Layout</h2>
-
+      <LoadingDialog loading={loading}/>
       {/* Basic info */}
 
       <CourseBasicInfo
@@ -66,7 +90,12 @@ const GenerateChapterContent =()=>{
         }}
       />
 
-      <Button className="my-10 flex justify-self-end" onClick={GenerateChapterContent}>Generate Course Content</Button>
+      <Button
+        className="my-10 flex justify-self-end"
+        onClick={GenerateChapterContent}
+      >
+        Generate Course Content
+      </Button>
     </div>
   );
 }
