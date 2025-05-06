50da52862e8c6715d2afbc7379f74d80600467af
longman14 <hassandamilola580@gmail.com>
2025-05-06 11:53:50 +0100

    bug fixes

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
 
 export default function Page() {
   return (
@@ -6,11 +7,8 @@ export default function Page() {
   <section className="bg-white">
     <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
       <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-        <img
-          alt=""
-          src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-          className="absolute inset-0 h-full w-full object-cover opacity-80"
-        />
+      <Image src="/welcome.jpg" alt='Sign in image' width={300} height={300} className='absolute inset-0 h-full w-full object-cover opacity-80'/>
+    
   
         <div className="hidden lg:relative lg:block lg:p-12">
           <a className="block text-white" href="#">
@@ -60,12 +58,11 @@ export default function Page() {
             </a>
   
             <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-              Welcome to Squid 🦑
+              Welcome to MattDacademy
             </h1>
   
             <p className="mt-4 leading-relaxed text-gray-500">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-              quibusdam aperiam voluptatum.
+              
             </p>
           </div>
   <SignIn/>
--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -7,15 +7,15 @@ function Header() {
   return (
     <div className="flex justify-between items-center px-5 py-1 shadow-md">
       <Link href={'/'}>
-      <Image src={'/logo.svg'} width={150} height={50} alt="Logo" />
+        <Image src={'/logo.svg'} width={100} height={50} alt="Logo" className="mr-auto" />
       </Link>
       <Link href={'/dashboard'} passHref>
         <Button
-          className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-secondary focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+          className="hidden md:block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-secondary focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
         >
           Get Started
         </Button>
-        </Link>
+      </Link>
     </div>
   );
 }
